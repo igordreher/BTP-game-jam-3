4c69cc14c8bdf137343fbb63034d5629c2a4949b
Igor Sparrenberger <igord.sparrenberger@gmail.com>
2021-01-23 13:03:33 -0300

    Add player movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float moveSpeed;
+    Rigidbody2D _rb;
+    Vector2 movementInput;
+
+    void Awake()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        movementInput = context.ReadValue<Vector2>();
+    }
+
+    public void OnFire(InputAction.CallbackContext context)
+    {
+    }
+
+    void FixedUpdate()
+    {
+        _rb.velocity = movementInput * moveSpeed;
+    }
+}
