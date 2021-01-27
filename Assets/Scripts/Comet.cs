5b324fa10dc59a4dd95e4fa0adea866bb62fc430
Igor Sparrenberger <igord.sparrenberger@gmail.com>
2021-01-27 18:43:07 -0300

    Create comet & comet spawner

---
+++ Assets/Scripts/Comet.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Comet : MonoBehaviour
+{
+    [SerializeField] float _moveSpeed;
+    Rigidbody2D _rb;
+    Vector2 _direction;
+
+    void Awake()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+        _direction = new Vector2(-1, -1);
+    }
+
+    void FixedUpdate()
+    {
+        _rb.velocity = _direction * _moveSpeed;
+    }
+}
---
+++ Assets/Scripts/CometSpawner.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CometSpawner : MonoBehaviour
+{
+    [SerializeField] float _spawnTime;
+    [SerializeField] GameObject _cometPrefab;
+    [SerializeField] float _spawnSize;
+    [SerializeField] Axis _axis;
+
+    void Start()
+    {
+        InvokeRepeating("SpawnComet", 0, _spawnTime);
+
+    }
+
+    void SpawnComet()
+    {
+        Vector3 spawnPoint;
+        if (_axis == 0)
+        {
+            var x1 = transform.position.x - _spawnSize / 2;
+            var x2 = transform.position.x + _spawnSize / 2;
+            spawnPoint = new Vector3(Random.Range(x1, x2), transform.position.y, 0);
+        }
+        else
+        {
+            var y1 = transform.position.y - _spawnSize / 2;
+            var y2 = transform.position.y + _spawnSize / 2;
+            spawnPoint = new Vector3(transform.position.x, Random.Range(y1, y2), 0);
+        }
+        Instantiate(_cometPrefab, spawnPoint, transform.rotation);
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        if (_axis == 0)
+            Gizmos.DrawWireCube(transform.position, new Vector3(_spawnSize, 1, 0));
+        else
+            Gizmos.DrawWireCube(transform.position, new Vector3(1, _spawnSize, 0));
+    }
+}
+
+public enum Axis
+{
+    Horizontal,
+    Vertical
+}
