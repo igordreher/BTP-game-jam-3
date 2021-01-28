09e3d012ff5d363e45fae9759ca58b99dbeae2ff
Igor Sparrenberger <igord.sparrenberger@gmail.com>
2021-01-28 18:32:03 -0300

    Update asteroids to destroy when far from player

--- Assets/Scripts/Asteroid.cs
+++ Assets/Scripts/Asteroid.cs
@@ -5,11 +5,21 @@ using UnityEngine;
 public class Asteroid : MonoBehaviour
 {
     [SerializeField] float _moveSpeed;
+    Transform _player;
     Rigidbody2D _rb;
+    float _distanceFromPlayer;
 
     void Awake()
     {
         _rb = GetComponent<Rigidbody2D>();
+        _player = GameObject.Find("Player").GetComponent<Transform>();
+    }
+
+    void Update()
+    {
+        _distanceFromPlayer = Vector3.Distance(transform.position, _player.position);
+        if (_distanceFromPlayer > 16)
+            Destroy(gameObject);
     }
 
     void FixedUpdate()
