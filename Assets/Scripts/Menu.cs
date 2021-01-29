492de08d273c8acb19fbec7549572ce291744985
Igor Sparrenberger <igord.sparrenberger@gmail.com>
2021-01-28 21:52:06 -0300

    Create menu

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    Scene _spaceScene;
+
+    void Awake()
+    {
+        _spaceScene = SceneManager.GetSceneByName("Space");
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(_spaceScene.name);
+    }
+}
